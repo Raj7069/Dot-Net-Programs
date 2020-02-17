ec17204460746f13d1974174154eb7d9db171692
Raj7069 <52092655+Raj7069@users.noreply.github.com>
2020-02-17 12:42:42 +0530

    Add files via upload

---
+++ Re/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Re
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name, Country;
+            do{
+            Console.WriteLine("Please, Enter your Name:-");
+            Name = Console.ReadLine();
+            }while(!Regex.IsMatch(Name,"^[a-zA-Z]+$"));
+
+            do{
+                Console.WriteLine("Please, Enter your Country:-");
+                Country = Console.ReadLine();
+
+            }while(!Regex.IsMatch(Country,"^[a-zA-Z]+$"));
+
+            Console.WriteLine("Hello, Your Name is "+Name+" and you from Country "+Country);
+            Console.Read();
+        }
+    }
+}
