6ec36da4593cca0114557c75be21fc0ce574b733
Raj7069 <rajpparmar1999@gmail.com>
2020-02-17 13:28:02 +0530

    Validation Using REGEX

--- Pattern/Program.cs
+++ Pattern/Program.cs
@@ -12,6 +12,7 @@ namespace Pattern
             pattern1();     //pattern 1
             pattern2();     //pattern 2
             pattern3();     //pattern 3
+            pattern4();     //pattern 4
         }
 
         static void pattern1()
@@ -56,8 +57,23 @@ namespace Pattern
                 Console.WriteLine();
             }
             Console.WriteLine();
+        }
+        static void pattern4()
+        {
+            Console.WriteLine("pattern 4");
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write("@");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
             Console.Read();
         }
+
+
     }
 
 }
---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = 42;
+            int y = 12;
+            int w;
+            object o;
+            o = x;
+            w = y * (int)o;
+            Console.WriteLine(w);
+        }
+    }
+}
