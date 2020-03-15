96fc915c5c4247bd6176ff052cb439027974fede
Raj7069 <rajpparmar1999@gmail.com>
2020-03-15 20:56:22 +0300

    convert currency in various form

---
+++ CurrencyConvertor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConvertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Enter the Currency in Rupee : ");
+            double currency = double.Parse(Console.ReadLine());
+            Console.WriteLine("You enter amount:- "+currency+ '\u20B9' + " and equivalent output is:- " + currency / 71.42 +'\u0024');
+            Console.WriteLine("You enter amount:- " + currency + '\u20B9' + " and equivalent output is:- " + currency / 0.68 + '\u00A5');
+            Console.WriteLine("You enter amount:- " + currency + '\u20B9' + " and equivalent output is:- " + currency / 82.84 + '\u20AC');
+            Console.ReadKey();
+        }
+    }
+}
